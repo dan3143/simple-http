2cc7c07311e4dcbe2dd81e11b748043d06646a48
vivilin <dasaing@proton.me>
2026-03-02 22:31:04 -0500

    separate server logic on a different file

---
+++ server.c
@@ -0,0 +1,109 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include "server.h"
+
+#define BACKLOG 10
+#define BUFFER_SIZE 1024
+
+void *get_in_addr(struct sockaddr *sa) {
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int init_server_sock(char *ipstr, char *port) {
+    int server_sockfd, status;
+    struct addrinfo *server_info, *p, hints;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    int yes = 1;
+
+    if ((status = getaddrinfo(ipstr, port, &hints, &server_info)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+        exit(1);
+    }
+
+    for (p = server_info; p != NULL; p = p->ai_next) {
+        if ((server_sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("server: socket");
+            continue;
+        }
+        if (setsockopt(server_sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+            perror("setsockopt");
+            exit(1);
+        }
+        if (bind(server_sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(server_sockfd);
+            perror("server: bind");
+            continue;
+        }
+        break;
+    }
+    
+    freeaddrinfo(server_info);
+
+    if (p == NULL) {
+        fprintf(stderr, "server: failed to bind\n");
+        exit(1);
+    }
+
+    printf("Initialized server on %s:%s\n", ipstr, port);
+
+    return server_sockfd;
+}
+
+void listen_on_server_sock(int server_sockfd) {
+    
+    if (listen(server_sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
+    
+    printf("Listening...\n");
+
+    struct sockaddr_storage client_addr;
+    socklen_t sin_size;
+    int client_sockfd;
+    int received_bytes;
+    char s[INET6_ADDRSTRLEN];
+    char buffer[BUFFER_SIZE];
+
+    while (1) {
+        sin_size = sizeof client_addr;
+        client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_addr, &sin_size);
+        if (client_sockfd == -1) {
+            perror("accept");
+            continue;
+        }
+        
+        inet_ntop(client_addr.ss_family, get_in_addr((struct sockaddr *)&client_addr), s, sizeof s);
+        printf("server: got connection from %s\n", s);
+
+        received_bytes = recv(client_sockfd, buffer, BUFFER_SIZE-1, 0);
+        if (received_bytes < 1) {
+            printf("error while reading from client %s\n", s);
+            close(client_sockfd);
+            continue;
+        }
+        buffer[received_bytes] = '\0';
+        if (send(client_sockfd, buffer, received_bytes, 0) == -1) perror("send");
+
+
+        printf("closing connection from %s\n", s);
+        close(client_sockfd);
+    }
+}
+
+void listen_on(char *ipstr, char *port) {
+    int server_sockfd = init_server_sock(ipstr, port);
+    listen_on_server_sock(server_sockfd);
+}
---
+++ server.h
@@ -0,0 +1,6 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+void listen_on(char*, char*);
+
+#endif
