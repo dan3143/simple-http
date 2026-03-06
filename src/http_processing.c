11f8a0104402eb796d3700f8fb7be574827cdf36
vivilin <dasaing@proton.me>
2026-03-06 14:06:32 -0500

    add configuration options for host, port, directory, and verbosity

--- src/http_processing.c
+++ src/http_processing.c
@@ -2,12 +2,6 @@
 #include "http_response.h"
 #include "utils.h"
 
-#define HTML_404 "404.html"
-#define ROOT_DIR "./static"
-#define METHOD_IDX 0
-#define ROUTE_IDX 1
-#define FILENAME_IDX 2
-
 void handle_http_request(int socketfd, char *buffer, size_t n_bytes) {
   send_error_response(socketfd, 400);
 }
--- src/main.c
+++ src/main.c
@@ -1,29 +1,46 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <arpa/inet.h>
-#include <string.h>
+#include "log.h"
 #include "server.h"
+#include <arpa/inet.h>
+#include <bits/getopt_core.h>
+#include <linux/limits.h>
+#include <netinet/in.h>
+#include <string.h>
 
-#define PORT "8080"
+typedef struct {
+  char host[INET6_ADDRSTRLEN];
+  char port[5];
+  char root_dir[PATH_MAX];
+  int log_level;
+} ServerConfig;
 
-void processArgs(char  *port, char *ipstr, int argc, char **argv) {
-    if (argc >= 2) {
-        strcpy(port, argv[1]);
+ServerConfig config = {.host = "0.0.0.0",
+                       .port = "8080",
+                       .root_dir = "./static",
+                       .log_level = LOG_INFO};
+
+void processArgs(int argc, char **argv) {
+  char c;
+  while ((c = getopt(argc, argv, "h:p:d:v")) != -1) {
+    switch (c) {
+    case 'h':
+      strncpy(config.host, optarg, INET6_ADDRSTRLEN);
+      break;
+    case 'p':
+      strncpy(config.port, optarg, 5);
+      break;
+    case 'd':
+      strncpy(config.root_dir, optarg, PATH_MAX);
+      break;
+    case 'v':
+      config.log_level = LOG_DEBUG;
+      break;
     }
-    if (argc >= 3) {
-        strcpy(ipstr, argv[2]);
-    }
-    if (argc >= 4) {
-        fprintf(stderr, "usage: http-test [port [address]]\n");
-        exit(0);
-    }
+  }
 }
 
 int main(int argc, char **argv) {
-    char port[5] = PORT;
-    char ipstr[INET6_ADDRSTRLEN] = "0.0.0.0";
-    processArgs(port, ipstr, argc, argv);
-    listen_on(ipstr, port);
-    return 0;
+  processArgs(argc, argv);
+  log_set_level(config.log_level);
+  listen_on(config.host, config.port);
+  return 0;
 }
-
