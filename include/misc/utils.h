b7ee9958ec804e55cbd5fb58b04b6bd072634569
Victor Guevara <vdguevara.31@gmail.com>
2026-03-08 16:38:48 -0500

    add function to dump strings in hex format

--- include/misc/utils.h
+++ include/misc/utils.h
@@ -12,6 +12,7 @@ char *get_file_extension(const char *);
 void get_addr_str(struct sockaddr *, char *);
 int get_port(struct sockaddr *);
 int send_all(int, char *, size_t *);
+void print_hex_bytes(char *, size_t);
 
 typedef struct {
   char host[INET6_ADDRSTRLEN];
